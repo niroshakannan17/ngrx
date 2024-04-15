aa4860552fe987f9a97962eca5ae2b8343399391
nirosha <niroshakannan17@gmail.com>
2024-04-15 17:01:13 +0530

    store created without any error

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerViewComponent } from './customer-view/customer-view.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { CustomerViewComponent } from './customer-view/customer-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    isDevMode() ? StoreDevtoolsModule.instrument() : []
   ],
   providers: [],
   bootstrap: [AppComponent]
